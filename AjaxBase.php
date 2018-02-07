ba67d7f82554727bf2aa21a48a8be72f178976e8
cjllove <cjllove@live.cn>
2018-02-07 16:56:44 +0800

    not finished

--- AjaxBase.php
+++ AjaxBase.php
@@ -1,98 +1,99 @@
 ﻿<?php
-//namespace FineUi_Js
+header("Content-Type:text/html;charset=utf-8;");
 
+
+//namespace FineUi_Js
      class AjaxBase
     {
 
-        public function ProcessRequest()
+        public static function ProcessRequest()
         {
-            if ($_GET["sMode"] != null && !string.IsNullOrEmpty($_GET["sMode"]) && $_GET["var1"]!=null)
+            if ($_GET["sMode"] != null &&  $_GET["var1"]!=null)
             {
-                
+                $con=mysqli_connect("localhost","cjl","cjl","cjl");
+                mysqli_set_charset($con,'utf8');
+
                  $mode = $_GET["sMode"];//模式
                  $var1=$_GET["var1"];//用户
                 //特定获取一级菜单
-                switch (mode)
+                switch ($mode)
                 {
                     case "memo":
-                        if (var1.Length > 0)
+                        if (strlen($var1) > 0)
                         {
-                             $sql = @"SELECT A.*
-                                    FROM SYS_FUNCROLE A
-                                    WHERE TREEID = '00'
-                                    ORDER BY ITEMSORT";
-
-                            DataTable dt = DbHelperOra.QueryForTable(sql);
-                            context.Response.ContentType = "text/plain";
-                            context.Response.Write(MyJsonPrint(dt));
+                            $sql = @"select * from (SELECT * FROM sys_funcrole WHERE treeid='00' )t
+                            ORDER BY t.funcid ";
+                            $result=mysqli_query($con,$sql);
+                            $arr=array();
+                            while($row = $result->fetch_assoc()) {  
+                                $count=count($row);//不能在循环语句中，由于每次删除row数组长度都减小  
+                                for($i=0;$i<$count;$i++){  
+                                    unset($row[$i]);//删除冗余数据  
+                                }  
+                                array_push($arr,$row);  
+                              
+                            }  
+                            //echo json_encode($arr,JSON_UNESCAPED_UNICODE);
+                            AjaxBase::MyJsonPrint($arr,true);
                         }
                         break;
                     case "memo2":
-                        if (v1.Length > 0)
+                        if (strlen($var1) > 0)
                         {
-                            String sql = @"SELECT *
-                                      FROM (SELECT *
-                                              FROM SYS_FUNCROLE
-                                             WHERE ROLEID = '00') T
-                                     START WITH T.FUNCID = '1'
-                                    CONNECT BY T.TREEID = PRIOR T.FUNCID
-                                     ORDER SIBLINGS BY ITEMSORT, FUNCID";
-
-                            DataTable dt = DbHelperOra.QueryForTable(sql);
-                            context.Response.ContentType = "text/plain";
-                            context.Response.Write(MyJsonPrint(dt));
+                            $sql = @"select * from (SELECT * FROM sys_funcrole WHERE treeid='00' AND FUNCID=1 )t
+     ORDER BY t.funcid ";
+                            $result=mysqli_query($con,$sql);
+                            $arr=array();
+                            while($row = $result->fetch_assoc()) {  
+                                $count=count($row);//不能在循环语句中，由于每次删除row数组长度都减小  
+                                for($i=0;$i<$count;$i++){  
+                                    unset($row[$i]);//删除冗余数据  
+                                }  
+                                array_push($arr,$row);  
+                              
+                            }  
+                            //echo json_encode($arr,JSON_UNESCAPED_UNICODE);
+                            AjaxBase::MyJsonPrint($arr,true);
                         }
                         break;
                     case "memo3":
-                        if (var1.Length > 0)
+                        if (strlen($var1) > 0)
                         {
-                            String sql = @"SELECT *
-                                      FROM (SELECT *
-                                              FROM SYS_FUNCROLE
-                                             WHERE ROLEID = '00') T
-                                     START WITH T.FUNCID = '{0}'
-                                    CONNECT BY T.TREEID = PRIOR T.FUNCID
-                                     ORDER SIBLINGS BY ITEMSORT, FUNCID";
+                            $sql = @" SELECT * FROM sys_funcrole
+                            WHERE  FIND_IN_SET(funcid, getChildLst($var1))  ";
+                       $result=mysqli_query($con,$sql);
+                       $arr=array();
+                       while($row = $result->fetch_assoc()) {  
+                           $count=count($row);//不能在循环语句中，由于每次删除row数组长度都减小  
+                        //    for($i=0;$i<$count;$i++){  
+                        //        unset($row[$i]);//删除冗余数据  
+                        //    }  
+                           array_push($arr,$row);  
+                         
+                       }  
+                       //echo json_encode($arr,JSON_UNESCAPED_UNICODE);
+                       AjaxBase::MyJsonPrint($arr,true);
 
-                            DataTable dt = DbHelperOra.QueryForTable(string.Format(sql, var1));
-                            context.Response.ContentType = "text/plain";
-                            context.Response.Write(MyJsonPrint(dt));
+                         
                         }
                         break;
                 }
             }
 
         }
-        // public static string MyJsonPrint(string str, bool flag = true)
-        // {
-        //     string res = "";
-        //     if (flag)
-        //     {
-        //         res = "{\"result\": \"succeed\",\"reason\": \"\",\"data\": \"" + str + "\"}";
-        //     }
-        //     else
-        //     {
-        //         res = "{\"result\": \"fail\",\"reason\": " + str + ",\"data\":\"\" }";
-        //     }
-        //     return res;
-        // }
-        public static string MyJsonPrint(DataTable dt, bool flag = true)
+      
+        public static function MyJsonPrint($ar, $flag )
         {
-            string res = "";
-            if (flag)
+             $res = "";
+            if ($flag)
             {
-                res = "{\"result\": \"succeed\",\"reason\": \"\",\"data\": " + JsonConvert.SerializeObject(dt) + "}";
+                $res = "{\"result\": \"succeed\",\"reason\": \"\",\"data\": ".json_encode($ar,JSON_UNESCAPED_UNICODE). "}";
             }
-            return res;
+            echo($res);
+            return $res;
         }
 
-        public bool IsReusable
-        {
-            get
-            {
-                return false;
-            }
-        }
+     
     }
-   
+   AjaxBase::ProcessRequest();
 ?>
