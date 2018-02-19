ed74daa82d48e516a628df2bc21da3a86ba2dd65
cjllove <cjllove@live.cn>
2018-02-19 21:26:23 +0800

    1

--- H/application/CCDictionary/controller/Goodsmanage.php
+++ H/application/CCDictionary/controller/Goodsmanage.php
@@ -5,6 +5,15 @@ use think\Db;
 use think\Request;
 class Goodsmanage extends controller{
     function index (){
+        $con=mysqli_connect("localhost","cjl","cjl","cjl");
+        $arrfun=array();
+        $result=mysqli_query($con,'select funcid,funcname,runwhat,micohelp
+        from sys_function');
+        while($row = $result->fetch_assoc()) { 
+              array_push($arrfun,$row);            
+        }  
+        $ss="{\"fields\": \"succeed\",\"reason\": \"\",\"data\": ".json_encode($arrfun,JSON_UNESCAPED_UNICODE). "}";
+        $this->assign('arrfun',$ss);
            return $this->fetch();
     }
 }
