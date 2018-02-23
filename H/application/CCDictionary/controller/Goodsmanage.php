dd089a02c654d4fede06290ba150cb5e0441abe8
cjllove <cjllove@live.cn>
2018-02-23 14:46:19 +0800

    1

--- H/application/CCDictionary/controller/Goodsmanage.php
+++ H/application/CCDictionary/controller/Goodsmanage.php
@@ -3,17 +3,12 @@ namespace app\CCDictionary\controller;
 use think\Controller;
 use think\Db;
 use think\Request;
+use cc\Grid;
 class Goodsmanage extends controller{
     function index (){
-        $con=mysqli_connect("localhost","cjl","cjl","cjl");
-        $arrfun=array();
-        $result=mysqli_query($con,'select funcid,funcname,runwhat,micohelp
-        from sys_function');
-        while($row = $result->fetch_assoc()) { 
-              array_push($arrfun,$row);            
-        }  
-        $ss="{\"fields\": \"succeed\",\"reason\": \"\",\"data\": ".json_encode($arrfun,JSON_UNESCAPED_UNICODE). "}";
-        $this->assign('arrfun',$ss);
+        $result=Db::table('sys_function')->select();//column('funcid','funcname','runwhat','micohelp');
+          $obj=new Grid();
+        $this->assign('arrfun',$obj->GridGetJson('funcid,funcname,runwhat,micohelp',$result));
            return $this->fetch();
     }
 }
